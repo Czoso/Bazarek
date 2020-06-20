3002dfa07b8cd0585aaf7fdc08697c9d5e688308
Michał Guziński <64793134+Czoso@users.noreply.github.com>
2020-06-20 21:44:23 +0200

    Add files via upload

--- Vendor.java
+++ Vendor.java
@@ -1,13 +1,20 @@
 package com.company;
 
-public class Vendor {
-    private String name;
+public class Vendor extends Human {
     private Product product;
     private double price;
-    public Vendor(String n, Product f, double p)
+    public Vendor(String name, Product f, double p)
     {
-        name=n;
+        super(name);
         product=f;
         price=p;
+    }
+    public void setPrice(int cash)
+    {
+        this.price=cash;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
